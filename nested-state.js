ea6e03303b26ceb36a51fdb17b8d6fae74030855
Harshad Gangurde <143880663+codeitharshad@users.noreply.github.com>
2025-04-26 22:58:28 +0530

    learn: Immer for immutability in reducer state updates

--- nested-state.js
+++ nested-state.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const produce = require("immer").produce;
+
 const initialState = {
   name: "Harshad",
   address: {
@@ -7,12 +10,41 @@ const initialState = {
   },
 };
 
-
 const STREET_UPDATED = "STREET_UPDATED";
 
 const updateSteet = (street) => {
-    return{
-        type: STREET_UPDATED,
-        payload: street,
-    }
-}
+  return {
+    type: STREET_UPDATED,
+    payload: street,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STREET_UPDATED:
+    //   return {
+    //     ...state,
+    //     address: {
+    //       ...state.address,
+    //       street: action.payload,
+    //     },
+    //   };
+
+    return produce(state,(draft) => {
+        draft.address.street = action.payload
+    })
+
+    default:
+      return state;
+  }
+};
+
+const store = redux.legacy_createStore(reducer);
+console.log("Initial State", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated State", store.getState());
+});
+
+store.dispatch(updateSteet("Peth Galli"));
+unsubscribe();
